e63624fd7e6aa5dccbd69d0418f85f270faeac27
Lucaslps10 <134088987+Lucaslps10@users.noreply.github.com>
2023-05-26 18:39:54 +0000

    set homepage for github pages deploy

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "Lucas Pereira da Silva",
+  title: "Desenvolvedor de conteúdo Web",
+  email: "lucas.silva.30@academico.ifpb.edu.br",
+  gitHub: "lucaslps10",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "microsoft",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
